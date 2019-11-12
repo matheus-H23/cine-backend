ac5ac3ad7acd00b3a6d3bd0d72d3c8677d4bd17e
Matheus Henrique Silva <matheushmms@hotmail.com>
2019-11-12 02:14:18 -0300

    Creating user Crud

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class UsersController < Api::V1::CrudController
 
-        before_action :set_user, only: %w['show']
+        before_action :set_user, only: %i[show update]
 
         def index
           @users = User.all
@@ -13,13 +13,23 @@ module Api
           render json: @user
         end
       
-        # def create
-        #   @ = .new(params[:])
-        # end
+        def create
+          @user = User.new(user_params)
+          if @user.save
+            render json: @user
+          else
+            render json: {status: "error", error: @user.errors.messages}, status: 401
+          end
+        end
       
-        # def update
-        #   @ = .find(params[:id])
-        # end
+        def update
+          binding.pry
+          if @user.update(user_params)
+            render json: @user
+          else
+            render json: {status: "error", message: "Error updating user, try again later", error: @user.errors.messages}, status: 500
+          end
+        end
       
         # def edit
         #   @ = .find(params[:id])
@@ -32,8 +42,11 @@ module Api
         private
 
         def set_user
-          binding.pry
           @user = User.find(params[:id])
+        end
+
+        def user_params
+          params.permit(:email, :firstname, :lastname, :password, :birthdate)
         end
     end
   end
