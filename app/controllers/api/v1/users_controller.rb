736436a3655c6985a7f593a57dbfd6e8b941b443
Matheus Henrique Silva <matheushmms@hotmail.com>
2019-11-20 10:57:44 -0300

    Adding destroy action to users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class UsersController < Api::V1::CrudController
 
-        before_action :set_user, only: %i[show update]
+        before_action :set_user, only: %i[show update destroy]
 
         def index
           @users = User.all
@@ -30,13 +30,13 @@ module Api
           end
         end
       
-        # def edit
-        #   @ = .find(params[:id])
-        # end
-      
-        # def destroy
-        #    = .find(params[:id])
-        # end
+        def destroy
+          if @user.destroy
+            render json: {status: "success", message: "User removed successfully"}, status: 200
+          else
+            render json: {status: "error", message: "Error destroying user, try again later", error: @user.errors.messages}, status: 500
+          end
+        end
 
         private
 
